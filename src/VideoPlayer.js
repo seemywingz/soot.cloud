10519376d36751d5494003648d47dff45bcc96a4
Kevin Jayne <kevin.jayne@icloud.com>
2018-09-22 14:02:13 -0400

    converter

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-// let movieFile = "movies/Horror/foo.mp4"
 let movieURL =  `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/watch`
 
 class VideoPlayer extends Component {
